6fa5acbc7418f6d0415e1b48b220aa8c9c94838c
Gedeon <jedionmelbin@gmail.com>
2018-09-16 11:23:44 -0500

    Curso de entity framework core.

---
+++ Capitulo04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Capitulo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
